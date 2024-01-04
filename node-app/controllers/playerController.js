58506b64ba499bb0132f8103a087684730ee0b54
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-01-04 22:23:38 +0530

    uppdated

--- node-app/controllers/playerController.js
+++ node-app/controllers/playerController.js
@@ -1,22 +1,23 @@
 const Player = require('../models/player');
 
 exports.getFirstPlayerDetails = async (req, res) => {
-  try {
-    // Fetch the first player from the database
-    const firstPlayer = await Player.findOne().sort({ _id: 1 }).exec();
-
-    if (firstPlayer) {
-      // Render the playerDetails.ejs view with the first player data
-      res.render('playerDetails', { player: firstPlayer });
-    } else {
-      // If there is no first player, redirect to the home page
-      res.redirect('/');
+    try {
+      // Fetch the first player from the database
+      const firstPlayer = await Player.findOne().sort({ _id: 1 }).exec();
+  
+      if (firstPlayer) {
+        // Render the correct view file ('playerDetails.ejs') with the first player data
+        res.render('playerDetails', { player: firstPlayer });
+      } else {
+        // If there is no first player, redirect to the home page
+        res.redirect('/');
+      }
+    } catch (error) {
+      console.error('Error fetching first player details:', error);
+      res.status(500).send('Error fetching first player details');
     }
-  } catch (error) {
-    console.error('Error fetching first player details:', error);
-    res.status(500).send('Error fetching first player details');
-  }
-};
+  };
+  
 
 exports.getNextPlayerDetails = async (req, res) => {
   try {
